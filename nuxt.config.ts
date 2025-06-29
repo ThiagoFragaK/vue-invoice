7eb4d29d54fead25d4d25cd24a381db534a012c9
ThiagoFragaK <thiagofraga@proton.me>
2025-06-29 19:13:00 -0300

    Add: Bootstrap and Lucide Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-05-15',
+    devtools: { 
+        enabled: true
+    },
+    css: [
+        '@/assets/main.scss' 
+    ],
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap'
+                }
+            ],
+            script: [
+                {
+                    src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',
+                    defer: true, 
+                },
+            ],
+        }
+    },
 })
