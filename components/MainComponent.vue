5113f3f069d80c769f8b1cfc9e784299ef3dc1e3
ThiagoFragaK <thiagofraga@proton.me>
2025-06-29 19:24:31 -0300

    Add: Download btn

--- components/MainComponent.vue
+++ components/MainComponent.vue
@@ -8,7 +8,13 @@
     <button
         class="btn btn-primary flex items-center justify-center gap-2 mt-4 ms-4"
     >
-        <LucideIcon icon="Download" />
+        <LucideIcon icon="Upload" />
         Import PDF
+    </button>
+    <button
+        class="btn btn-info flex items-center justify-center gap-2 mt-4 ms-4"
+    >
+        <LucideIcon icon="Download" />
+        Download Invoice
     </button>
 </template>
