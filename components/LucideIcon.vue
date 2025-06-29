0a0bd058b04cd898a64f6e3d87ae6bcbb0210bea
ThiagoFragaK <thiagofraga@proton.me>
2025-06-29 19:13:30 -0300

    Add: Lucide component and plugin

---
+++ components/LucideIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <component :is="iconComponent" v-bind="$attrs" size="20" />
+  </template>
+  
+<script>
+    import * as LucideIcons from "lucide-vue-next";
+    export default {
+        props: {
+            icon: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            iconComponent() {
+                return LucideIcons[this.icon] || null;
+            }
+        }
+    };
+</script>
+  
