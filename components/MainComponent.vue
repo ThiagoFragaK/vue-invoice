9a46afcdd402b3242ae067f1a4e06baa8ff3c8a2
ThiagoFragaK <thiagofraga@proton.me>
2025-06-29 19:13:56 -0300

    Add: Main and Navbar components

---
+++ components/MainComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <button
+        class="btn btn-primary flex items-center justify-center gap-2 mt-4 ms-4"
+    >
+        <LucideIcon icon="FilePlus2" />
+        Create Invoice
+    </button>
+    <button
+        class="btn btn-primary flex items-center justify-center gap-2 mt-4 ms-4"
+    >
+        <LucideIcon icon="Download" />
+        Import PDF
+    </button>
+</template>
